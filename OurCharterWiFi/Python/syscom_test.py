140c0ef4e2f72c9d64909ae212bdffcd7c5533b6
edwardayala <47757464+edwardayala@users.noreply.github.com>
2020-02-10 02:48:20 +0000

    Add files via upload

--- OurCharterWiFi/Python/syscom_test.py
+++ OurCharterWiFi/Python/syscom_test.py
@@ -1,12 +1,33 @@
 import subprocess as sp
+import time as t
+import signal
+import sys
+from os import system
+#print('test subprocess library')
 
+#file = 'output.txt'
 
-print('test subprocess library')
+#sp.call(['ls','-l'])
+#sp.call(['touch',file])
+#sp.call(['vi',file])
 
-file = 'output.txt'
+#print('test complete')
+print('test aircrack-ng suite')
+# KILL INTERFERING PROCESSES
+print('KILLING TASKS')
+sp.run(['airmon-ng','check','kill'], capture_output=True)
+print('TASKS KILLED')
+t.sleep(3)
+# SET WIRELESS CARD TO MONITOR MODE
+print('ENTERING MONITOR MODE')
+sp.run(['airmon-ng','start','wlan0'], capture_output=True)
+t.sleep(3)
+print('CHECKING WIRELESS CARD STATUS')
+sp.run('iwconfig')
+t.sleep(1)
+sp.run('NetworkManager')
+# SCAN FOR NEARBY NETWORKS
+sys.exit(0)
+sp.run(['airodump-ng','wlan0mon'])
+t.sleep(10)
 
-sp.call(['ls','-l'])
-sp.call(['touch',file])
-sp.call(['vi',file])
-
-print('test complete')
