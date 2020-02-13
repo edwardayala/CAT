77425f6355716577df3b1065c16d754532e860c9
edwardayala <47757464+edwardayala@users.noreply.github.com>
2020-02-13 22:21:48 +0000

    Update syscom_test.py

--- OurCharterWiFi/Python/syscom_test.py
+++ OurCharterWiFi/Python/syscom_test.py
@@ -3,31 +3,22 @@ import time as t
 import signal
 import sys
 from os import system
-#print('test subprocess library')
 
-#file = 'output.txt'
-
-#sp.call(['ls','-l'])
-#sp.call(['touch',file])
-#sp.call(['vi',file])
-
-#print('test complete')
-print('test aircrack-ng suite')
 # KILL INTERFERING PROCESSES
-print('KILLING TASKS')
-sp.run(['airmon-ng','check','kill'], capture_output=True)
-print('TASKS KILLED')
+print('************KILLING TASKS************')
+sp.run(['airmon-ng', 'check', 'kill'])
+print('************TASKS KILLED************')
 t.sleep(3)
+
 # SET WIRELESS CARD TO MONITOR MODE
-print('ENTERING MONITOR MODE')
-sp.run(['airmon-ng','start','wlan0'], capture_output=True)
+print('************ENTERING MONITOR MODE************')
+sp.run(['airmon-ng', 'start', 'wlan0'])
 t.sleep(3)
-print('CHECKING WIRELESS CARD STATUS')
+
+print('************CHECKING WIRELESS CARD STATUS************')
 sp.run('iwconfig')
 t.sleep(1)
 sp.run('NetworkManager')
 # SCAN FOR NEARBY NETWORKS
-sys.exit(0)
-sp.run(['airodump-ng','wlan0mon'])
+sp.run(['airodump-ng', 'wlan0mon'])
 t.sleep(10)
-
