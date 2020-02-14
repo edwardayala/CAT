3aeb6c7525a6373376ef6db9a8ff1ba9bc672710
edwardayala <47757464+edwardayala@users.noreply.github.com>
2020-02-14 22:24:53 +0000

    Changed subprocess stuff
    from subprocess.Popen() to subprocess.run()

--- OurCharterWiFi/Python/syscom_test.py
+++ OurCharterWiFi/Python/syscom_test.py
@@ -29,35 +29,39 @@ def welcome():
     print('  ▀▀         ▀▀██████▀▀   ▀▀██  ')                                                                                                                      
     print(Style.RESET_ALL)
     
-
-def checkInterface():
-    checkInterfaceProc = sp.Popen([ROOT,'iwconfig'], stdout=sp.PIPE)    # Runs 'sudo iwconfig'
-    iwconfig_output = checkInterfaceProc.stdout.read().decode('utf-8')  # save output to variable | use .decode to convert byte to string
-    if (iwconfig_output.find(interfaceMonitor) > 0):                    # if the interface is in Monitor mode, good, if not then toggle settings
-        print('Interface is in Monitor mode - GOOD TO GO')
-    else:
-        print('Interface is not in Monitor mode - Changing to Monitor Mode...')
-        monitorToggle(interfaceManaged, 0)
-
 def checkProcesses():
-    checkProcessesProc = sp.Popen([ROOT,'airmon-ng','check'], stdout=sp.PIPE)
-    checkProc_output = checkProcessesProc.stdout.read().decode('utf-8')
-    # TODO: check if airmon-ng check has output 
-    if (checkProc_output.find('F')):
-        print('Found interfering processes - Killing Processes \n This will disconnect you from the Internet')
-        # KILL
+    checkProcessesProc = sp.run([ROOT,'airmon-ng','check'], capture_output=True, text=True)
+    if (checkProcessesProc.stdout.find('F')):
+        print(Fore.RED,'Found interfering processes',Fore.BLUE,'\n\n Killing processes will disconnect you from the Internet, would you like to continue? (y/n)',Style.RESET_ALL)
+        usrInput = input()
+        if usrInput == 'y' or usrInput == 'yes':
+            print('Killing processes...')
+            sp.run([ROOT,'airmon-ng','check','kill'], capture_output=True)
+        elif usrInput == 'n' or usrInput == 'no':
+            print('Attempting to toggle Monitor mode, this may not work without killing the interfering processes.')
     else:
         print('No interfering processes - GOOD TO GO')
 
-def monitorToggle(interface, mode):                                     # Runs airmon-ng to start/stop Monitor mode
-    sp.run([ROOT,'airmon-ng','check','kill'])                           # Kills interfering processes
-    if mode == 0:
-        sp.run([ROOT,'airmon-ng','start',interface])                    # Toggle Monitor Mode
+def checkInterface():
+    checkInterfaceProc = sp.run([ROOT,'iwconfig'], capture_output=True, text=True)    # Runs 'sudo iwconfig'
+    if (checkInterfaceProc.stdout.find(interfaceMonitor) > 0):                    # if the interface is in Monitor mode, good, if not then toggle settings
+        print('Interface is in Monitor mode - GOOD TO GO')
     else:
-        sp.run([ROOT,'airmon-ng','stop',interface])                     # Toggle Managed Mode
-        sp.run([ROOT,'NetworkManager'])                                 # Restart NetworkMananger to connect to Internet
+        print('Interface is not in Monitor mode - Changing to Monitor Mode...')
+        checkProcesses()
+        # monitorToggle(interfaceManaged, 0)
 
-welcome()
+def monitorToggle(interface, mode):     # Runs airmon-ng to start/stop Monitor mode
+    # checkProcesses()
+    if mode == 0:
+        sp.run([ROOT,'airmon-ng','start',interface], capture_output=True)   # Toggle Monitor Mode
+    else:
+        sp.run([ROOT,'airmon-ng','stop',interface], capture_output=True)    # Toggle Managed Mode
+        sp.run([ROOT,'NetworkManager'])     # Restart NetworkMananger to connect to Internet
+
+welcome()       # prints title 
+
+
 
 # # KILL INTERFERING PROCESSES
 # print('************KILLING TASKS************')
